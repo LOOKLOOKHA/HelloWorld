6e8e1f7e8443814d1dc6843ae49ad4a1cd255897
zhaoxing <511408615@qq.com>
2017-07-18 22:58:05 +0800

    aa

--- gitWeb/src/gitWeb/gitjava.java
+++ gitWeb/src/gitWeb/gitjava.java
@@ -5,7 +5,6 @@ import org.eclipse.jdt.internal.compiler.batch.Main;
 public class gitjava {
 	public static void main(String[] args) {
 		System.out.println("55");
-		System.out.println("55");
-		System.out.println("55");fgf
+	 
 	}
 }
